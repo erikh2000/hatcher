e95035068f08db099c1d94993bf5102bf3008fcc
Erik Hermansen <info@seespacelabs.com>
2017-05-26 23:34:08 -0700

    Fix some bugs.

--- src/density-map.js
+++ src/density-map.js
@@ -1,8 +1,6 @@
 import { ContainsPoint as containsPoint } from 'polyk';
 
 export const getDensityAtPoint = ({x, y, defaultDensity, densityZones}) => {
-  densityZones.forEach( (zone) => {
-    if (containsPoint(zone.polygon, x, y)) { return zone.density; }
-  });
-  return defaultDensity;
+  const containingZone = densityZones && densityZones.find( (zone) => containsPoint(zone.polygon, x, y) );
+  return containingZone ? containingZone.density : defaultDensity;
 };
